1224b6aae1c99cedd85954e85bc41ac301e012fa
wuhao <wuhao@aegis-data.cn>
2020-11-14 15:38:55 +0800

    '修复类型错误'

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,12 @@
+import {AxiosRequestConfig} from 'axios';
+import {API, ApiDef, ApiObject, AppConfig} from './api';
+
 export * from './api';
 
 export * from '../src/types/global';
+
+declare function apiProxy(apiObj: ApiObject<ApiDef>,
+                          config: AppConfig,
+                          commonSettings: AxiosRequestConfig): ApiObject<API>;
+
+export default apiProxy;
